4c4159d51db72c93d6c6bdaa385b7b22e1062112
adlo <adloconwy@gmail.com>
2015-07-08 19:52:13 +0100

    First commit

---
+++ panel-plugin/sample.h
@@ -0,0 +1,58 @@
+/*  $Id$
+ *
+ *  Copyright (C) 2015 adlo
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __SAMPLE_H__
+#define __SAMPLE_H__
+#include <libwnck/libwnck.h>
+
+G_BEGIN_DECLS
+
+/* plugin structure */
+typedef struct
+{
+    XfcePanelPlugin *plugin;
+
+    /* panel widgets */
+    GtkWidget       *ebox;
+    GtkWidget       *hvbox;
+    GtkWidget       *label;
+    GtkWidget       *icon;
+    GdkPixbuf       *pixbuf;
+    GtkWidget       *menu;
+    GtkWidget       *action_menu;
+    
+    WnckScreen      *screen;
+    WnckWindow      *wnck_window;
+
+    /* sample settings */
+    gchar           *setting1;
+    gint             setting2;
+    gboolean         setting3;
+}
+SamplePlugin;
+
+
+
+void
+sample_save (XfcePanelPlugin *plugin,
+             SamplePlugin    *sample);
+
+G_END_DECLS
+
+#endif /* !__SAMPLE_H__ */
