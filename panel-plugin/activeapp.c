93f6c30202d67db3681d3305c227aabd666cc502
adlo <adloconwy@gmail.com>
2018-05-03 03:19:46 +0100

    Update year

--- panel-plugin/activeapp.c
+++ panel-plugin/activeapp.c
@@ -1,6 +1,6 @@
 /*  $Id$
  *
- *  Copyright (C) 2015 adlo
+ *  Copyright (C) 2015, 2018 adlo
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
